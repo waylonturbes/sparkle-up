20775480f38aec72fcadf838f1343680cca5e369
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-23 09:59:00 -0700

    style: update theme, footer, and app bar

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ let sparkleUp = new Vue({
     theme: {
       themes: {
         light: {
-          primary: "#3f51b5",
-          secondary: "#b0bec5",
-          accent: "#8c9eff",
-          error: "#b71c1c",
+          primary: "#1976D2",
+          secondary: "#424242",
+          accent: "#82B1FF",
+          error: "#FF5252",
+          info: "#2196F3",
+          success: "#4CAF50",
+          warning: "#FFC107",
         },
       },
     },
