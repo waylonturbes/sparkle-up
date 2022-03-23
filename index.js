cd49a77ac7456bf1a841ae9be072453045e1e224
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-22 20:00:30 -0700

    feat: add and initialize vuetify

---
+++ index.js
@@ -0,0 +1,18 @@
+let sparkleUp = new Vue({
+  el: "#sparkle-up",
+  vuetify: new Vuetify({
+    theme: {
+      themes: {
+        light: {
+          primary: "#3f51b5",
+          secondary: "#b0bec5",
+          accent: "#8c9eff",
+          error: "#b71c1c",
+        },
+      },
+    },
+  }),
+  data: {
+    appName: "Sparkle Up",
+  },
+});
