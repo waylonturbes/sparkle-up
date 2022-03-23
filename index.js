be0812a4136bc620e5be07e94da0fb8911ce80c5
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-23 08:15:37 -0700

    feat: add a footer to app content

--- index.js
+++ index.js
@@ -14,46 +14,63 @@ let sparkleUp = new Vue({
   }),
   data: {
     appName: "Sparkle Up",
+    icons: [
+      {
+        svg: "mdi-linkedin",
+        link: "https://www.linkedin.com/in/waylon-turbes/",
+      },
+      { svg: "mdi-github", link: "https://github.com/waylonturbes" },
+    ],
     sparklingWaters: [
       {
         flavor: "Lemon",
         brand: "La Croix",
+        rating: 10,
       },
       {
         flavor: "Grape",
         brand: "bubly",
+        rating: 5,
       },
       {
         flavor: "Orange",
         brand: "Polar",
+        rating: 8,
       },
       {
         flavor: "Cherry",
         brand: "Waterloo",
+        rating: 6,
       },
       {
         flavor: "Green Apple",
         brand: "Schweppes",
+        rating: 5,
       },
       {
         flavor: "Lime",
         brand: "Simply Balanced",
+        rating: 7,
       },
       {
         flavor: "Cucumber",
         brand: "Spindrift",
+        rating: 6,
       },
       {
         flavor: "Pink Grapefruit",
         brand: "Perrier",
+        rating: 5,
       },
       {
         flavor: "Citrus",
         brand: "Adirondack",
+        rating: 6,
       },
       {
         flavor: "Pomegranate Lemonade",
         brand: "Perrier",
+        rating: 8,
       },
     ],
   },
