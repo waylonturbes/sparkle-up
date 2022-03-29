242626d43c3badfa92f8b24d8c5635f4ca796fc6
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-29 14:57:44 -0700

    feat: create and attach navbar to app

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <sparkle-up-nav-bar />
     <v-main>
       <router-view />
     </v-main>
@@ -42,9 +10,10 @@
 
 <script>
 import SparkleUpFooter from "./components/SparkleUpFooter.vue";
+import SparkleUpNavBar from "./components/SparkleUpNavBar.vue";
 
 export default {
-  components: { SparkleUpFooter },
+  components: { SparkleUpFooter, SparkleUpNavBar },
   name: "App",
   data: () => ({}),
 };
---
+++ src/components/SparkleUpNavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar
+    app
+    short
+    class="primary"
+    style="
+      position: sticky;
+      display: flex;
+      align-items: center;
+      max-height: 60px;
+    "
+  >
+    <h1 class="text-h4 font-weight-bold white--text">Sparkle Up</h1>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "SparkleUpNavBar",
+  data() {
+    return {};
+  },
+};
+</script>
