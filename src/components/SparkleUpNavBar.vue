dc832dfb938a3b37a82cf07f04948003a2e7c3b4
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-03-30 10:21:45 -0700

    style: make footer not sticky, remove nav-bar elevation

--- src/components/SparkleUpNavBar.vue
+++ src/components/SparkleUpNavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar app short class="primary">
+  <v-app-bar app :elevation="0" color="primary">
     <h1 class="text-h4 font-weight-bold white--text">Sparkle Up</h1>
   </v-app-bar>
 </template>
