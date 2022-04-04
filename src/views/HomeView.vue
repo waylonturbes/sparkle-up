1b523766bb00e8cbef066b97884445a3758f9363
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-04-04 14:00:00 -0700

    style: rearrange home page content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,32 @@
     color="rgba(0, 0, 0, 0)"
     class="d-flex flex-column justify-space-around my-10"
   >
-    <v-row justify="center" class="mb-10">
+    <v-row justify="center">
       <v-col cols="6">
         <h1 class="text-h2 text-center">
           Find, create, <br />
           rate & review <br />
           sparkling waters.
         </h1>
+      </v-col>
+    </v-row>
+    <v-row justify="center" class="mb-10 mb-md-0">
+      <v-col md="6" cols="8" class="justify-center">
+        <spinning-can-animation />
+      </v-col>
+      <v-col md="6" align-self="center" cols="8">
+        <p>
+          Want to record sparkling waters, but don't want to write them all down
+          by hand, on a sheet of paper? Well, Sparkle Up has what you need. With
+          Sparkle Up, you can record and rate/review your favorite (or least
+          favorite) sparkling waters in just seconds.
+        </p>
+        <v-btn
+          color="primary"
+          x-large
+          v-text="'get started'"
+          to="/sparkling-waters"
+        ></v-btn>
       </v-col>
     </v-row>
     <v-row justify="center">
@@ -23,27 +42,6 @@
           ></v-carousel-item>
         </v-carousel>
       </v-card>
-    </v-row>
-    <v-row justify="center">
-      <spinning-can-animation style="height: 320px; width: 320px" />
-    </v-row>
-    <v-row justify="center">
-      <v-col cols="8">
-        <p class="text-center">
-          Want to record sparkling waters, but don't want to write them all down
-          by hand, on a sheet of paper? Well, Sparkle Up has what you need. With
-          Sparkle Up, you can record and rate/review your favorite (or least
-          favorite) sparkling waters in just seconds.
-        </p>
-      </v-col>
-    </v-row>
-    <v-row justify="center">
-      <v-btn
-        color="primary"
-        x-large
-        v-text="'get started'"
-        to="/sparkling-waters"
-      ></v-btn>
     </v-row>
   </v-container>
 </template>
