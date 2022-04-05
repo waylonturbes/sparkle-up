ed27700a96e57159c53da498e31da4abc192dfbf
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-04-05 09:03:31 -0700

    feat: responsive about page, new content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,45 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="12" sm="6">
-        <h1 class="text-h2 text-center">About Sparkle Up</h1>
+  <v-container
+    color="rgba(0, 0, 0, 0)"
+    class="d-flex flex-column justify-space-around my-10"
+  >
+    <v-row justify="center" class="mb-10">
+      <v-col cols="10" sm="8">
+        <h1 class="text-h2 text-center">
+          About
+          <br v-if="$vuetify.breakpoint.sm || $vuetify.breakpoint.xs" />
+          Sparkle Up
+        </h1>
       </v-col>
     </v-row>
     <v-row justify="center">
-      <v-col cols="6">
-        <CarouselCard :images="sparklingWaterImages" height="600px" />
+      <v-col cols="12" lg="5">
+        <CarouselCard
+          :images="sparklingWaterImages"
+          :height="
+            $vuetify.breakpoint.sm || $vuetify.breakpoint.md ? '600px' : '400px'
+          "
+        />
+      </v-col>
+      <v-col cols="12" lg="5" align-self="center">
+        <p class="text-body1 text-justify">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut iusto
+          praesentium officiis, ex nobis quaerat sit consequuntur perspiciatis,
+          doloremque quibusdam eum dignissimos vitae. Voluptatum totam deleniti
+          velit explicabo. Architecto iusto corporis atque, minus sequi porro
+          animi tempore vel voluptate, ratione mollitia nihil beatae iste odio
+          non ad provident? Molestiae saepe laudantium soluta totam explicabo.
+          Laborum adipisci exercitationem similique veritatis quaerat quam nemo
+          culpa pariatur. Architecto soluta aliquid ullam eveniet, quae sit,
+          inventore quis, quidem explicabo maiores natus dignissimos! Culpa
+          itaque dolore consectetur possimus cum? At laborum corporis ut iusto
+          deleniti! Dolorum velit esse aperiam itaque iure est quasi cum maiores
+          id. Amet voluptatibus et facilis consectetur adipisci quisquam! Ipsam,
+          at. Atque, impedit id culpa nesciunt fugiat dolore illo facere odit
+          repellat maiores laboriosam iste molestiae! Dolores culpa earum
+          officiis magni, in obcaecati repellendus doloribus nihil
+          reprehenderit.
+        </p>
       </v-col>
     </v-row>
   </v-container>
